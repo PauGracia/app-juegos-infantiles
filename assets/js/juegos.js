9f25f72c52fa14fe211e290a76a62c5fe9f2b2b2
PauGracia <paugracia7@gmail.com>
2026-01-21 12:19:46 +0100

    mejora css de menu principal

--- assets/js/juegos.js
+++ assets/js/juegos.js
@@ -1,2 +1,27 @@
-// js de todos los juegos
+const settingsBtn = document.getElementById("settings-btn");
+const settingsMenu = document.getElementById("settings-menu");
 
+settingsBtn.addEventListener("click", () => {
+  settingsMenu.style.display =
+    settingsMenu.style.display === "block" ? "none" : "block";
+});
+
+document
+  .getElementById("btn-settings-language")
+  .addEventListener("click", () => {
+    settingsMenu.style.display = "none";
+    document.getElementById("modal-language").style.display = "flex";
+  });
+
+document
+  .getElementById("btn-settings-objective")
+  .addEventListener("click", () => {
+    settingsMenu.style.display = "none";
+    document.getElementById("modal-objective").style.display = "flex";
+  });
+
+function cerrarModalSettings() {
+  document.querySelectorAll(".modal-settings").forEach((m) => {
+    m.style.display = "none";
+  });
+}
